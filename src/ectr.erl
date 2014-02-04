2cb3ba19c6a314762b7a903b1578e43e7b4b8f2e
Geoff Cant <nem@erlang.geek.nz>
2014-02-04 15:50:49 -0800

    s/tid/tab/g.

--- src/ectr.erl
+++ src/ectr.erl
@@ -39,26 +39,26 @@ start_link(Name,
                         ReportFunction,
                         IntervalMS).
 
--spec incr(Tid::ets:tid(),
+-spec incr(Tab::ets:tab(),
            Key::counter_key()) -> any().
 incr(Name, Key) ->
     incr(Name, Key, 1).
 
--spec incr(Tid::ets:tid(),
+-spec incr(Tab::ets:tab(),
            Key::counter_key(),
            Increment::integer()) -> any().
 
-incr(Tid, Key, Incr)
-  when is_atom(Tid) orelse is_integer(Tid),
+incr(Tab, Key, Incr)
+  when is_atom(Tab) orelse is_integer(Tab),
        is_integer(Incr) ->
-    try ets:update_counter(Tid, Key, Incr)
+    try ets:update_counter(Tab, Key, Incr)
     catch error:badarg -> % Usually this is because the key doesn't exist.
-            try ets:insert_new(Tid, {Key, Incr})
+            try ets:insert_new(Tab, {Key, Incr})
             catch error:badarg -> % We lost the key creation race, just update.
                     %% Here, the catch allows us to avoid crashing if
                     %% the table really doesn't exist. This means the
                     %% table owning process can be restarted and
                     %% callers will be able to make progress.
-                    catch ets:update_counter(Tid, Key, Incr)
+                    catch ets:update_counter(Tab, Key, Incr)
             end
     end.
